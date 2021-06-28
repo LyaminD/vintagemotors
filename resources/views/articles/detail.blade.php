b4bcd08f80795d7b35fedab3919b7d73544b42ad
LyaminD <Lyamin.diafat@sfr.fr>
2021-06-28 17:02:31 +0200

    Update detail.blade.php

--- resources/views/articles/detail.blade.php
+++ resources/views/articles/detail.blade.php
@@ -9,7 +9,7 @@
             <h5 class="card-title"> {{ $article->nom}}</h5>
             <p class="card-text"> {{ $article->description}}</p>
             <p class="card-text"> {{ $article->description_detaillee}}</p>
-            <p class="card-text"> {{ $article-><?php $stockcolor ?>}}</p>
+            <p class="card-text"> @php Helpers($article->stock) @endphp</p>
             <p class="card-text"> {{ $article->prix}} Euros</p>
             <p class="card-text"> {{ $article->note}}</p>
             <a href="#" class="btn btn-primary">Acheter</a>
